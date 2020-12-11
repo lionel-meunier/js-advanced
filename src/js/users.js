a38e205c3ad10c4f824754369239341d3db9cd51
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-11 14:09:00 +0100

    Add users and comment

---
+++ src/js/users.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    id: 1,
+    createdAt: "2020-02-12T04:41:58.971Z",
+    name: "Leonard Walsh",
+    address: {
+      city: "Boylehaven",
+      country: "Comoros",
+    },
+  },
+  {
+    id: 2,
+    createdAt: "2020-07-07T00:33:58.010Z",
+    name: "Thora Bailey",
+    address: {
+      city: "East Taniaport",
+      country: "Philippines",
+    },
+  },
+  {
+    id: 3,
+    createdAt: "2019-10-13T01:15:08.609Z",
+    name: "Geraldine Breitenberg",
+    address: {
+      city: "Aylaview",
+      country: "Syrian Arab Republic",
+    },
+  },
+  {
+    id: 4,
+    createdAt: "2019-10-13T01:15:08.609Z",
+    name: "Marcel Breitenberg",
+    address: {
+      city: "Aylaview",
+      country: "Syrian Arab Republic",
+    },
+  },
+];
