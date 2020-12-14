bce75a562f60f24228df692e7c861bb5e11ee673
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-14 15:26:25 +0100

    Add get token

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,17 @@
 fetch("https://api.jsonapi.co/rest/v1/user/list?limit=50")
   .then((res) => res.json())
   .then((res) => sortUsersByNeighbor(res.data.users));
+
+fetch("https://api.jsonapi.co/rest/v1/user/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    email: "toto",
+    password: "toto",
+  }),
+})
+  .then((res) => res.json())
+  .then((res) => res.data.token)
+  .then((res) => console.log(res));
