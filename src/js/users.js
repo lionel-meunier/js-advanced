f2dd79d07457e7cfc5a71f4b8dcb60fe0993d8f8
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-14 13:18:54 +0100

    refactor user for use function

--- src/js/users.js
+++ src/js/users.js
@@ -1,79 +1,3 @@
-const users = [
-  {
-    id: 1,
-    createdAt: "2020-02-12T04:41:58.971Z",
-    name: "Leonard Walsh",
-    address: {
-      city: "Boylehaven",
-      country: "Comoros",
-      latitude: 43.619891,
-      longitude: 6.9195798,
-    },
-  },
-  {
-    id: 2,
-    createdAt: "2020-07-07T00:33:58.010Z",
-    name: "Thora Bailey",
-    address: {
-      city: "East Taniaport",
-      country: "Philippines",
-      latitude: 43.649891,
-      longitude: 3.9195798,
-    },
-  },
-  {
-    id: 3,
-    createdAt: "2019-10-13T01:15:08.609Z",
-    name: "Geraldine Breitenberg",
-    address: {
-      city: "Aylaview",
-      country: "Syrian Arab Republic",
-      latitude: 43.619591,
-      longitude: 3.9195798,
-    },
-  },
-  {
-    id: 4,
-    createdAt: "2019-10-13T01:15:08.609Z",
-    name: "Marcel Breitenberg",
-    address: {
-      city: "Aylaview",
-      country: "Syrian Arab Republic",
-      latitude: 45.619891,
-      longitude: 3.9195798,
-    },
-  },
-];
-
-/**
- * Trier les utilisateurs par createdAt
- */
-const userSortedByDate = users.sort((a, b) => {
-  if (a.createdAt > b.createdAt) {
-    return 1;
-  } else if (b.createdAt > a.createdAt) {
-    return -1;
-  } else {
-    return 0;
-  }
-});
-
-/**
- * Trier les utilisateurs par createdAt en utilisant moment
- */
-const userSortedByDateWithMoment = users
-  .sort((a, b) => {
-    const aDate = moment(a.createdAt);
-    const bDate = moment(b.createdAt);
-    return aDate.diff(bDate);
-  })
-  .map((u) => u.name);
-
-console.log(
-  "user name sorted by date with momentjs",
-  userSortedByDateWithMoment
-);
-
 class Address {
   constructor(data) {
     this.city = data.city;
@@ -121,16 +45,19 @@ class User {
   }
 }
 
-const usersClass = users
-  .map((userData) => new User(userData))
-  .map((currentUser, index, usersInstance) => {
-    usersInstance.forEach((userCompare) => {
-      if (currentUser !== userCompare && currentUser.isNeighbor(userCompare)) {
-        currentUser.neighbor.add(userCompare);
-      }
-    });
-    return currentUser;
-  })
-  .sort((a, b) => b.neighbor.size - a.neighbor.size);
-
-console.log("User instance sorted by neighbor", usersClass);
+function sortUsersByNeighbor(users) {
+  return users
+    .map((userData) => new User(userData))
+    .map((currentUser, index, usersInstance) => {
+      usersInstance.forEach((userCompare) => {
+        if (
+          currentUser !== userCompare &&
+          currentUser.isNeighbor(userCompare)
+        ) {
+          currentUser.neighbor.add(userCompare);
+        }
+      });
+      return currentUser;
+    })
+    .sort((a, b) => b.neighbor.size - a.neighbor.size);
+}
