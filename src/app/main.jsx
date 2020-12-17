1b3e81def0a9fb19869695a6ec7dd40bf1ad14fd
Lionel MEUNIER <lmeunier@iocean.fr>
2020-12-17 09:40:12 +0100

    Fix error key in main.jsx

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -7,17 +7,17 @@ import { Users } from "./components/Users.js";
 const domContainer = document.querySelector("#root");
 
 const hello = (
-  <div className="card">
+  <div className="card" key={0}>
     <div className="card-body">
-      <HelloButton initCount={0}></HelloButton>
-      <HelloButton initCount={2}></HelloButton>
-      <HelloButton initCount={3}></HelloButton>
+      <HelloButton key={0} initCount={0}></HelloButton>
+      <HelloButton key={1} initCount={2}></HelloButton>
+      <HelloButton key={2} initCount={3}></HelloButton>
     </div>
   </div>
 );
 
 const usersArray = (
-  <div className="card">
+  <div className="card" key={1}>
     <div className="card-body">
       <Users></Users>
     </div>
